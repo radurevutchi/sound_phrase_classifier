b9fb0d5bf70f334b2885ec9e5016c147a98e302b
Radu Stefan Revutchi <radurevutchi@gmail.com>
2018-08-01 17:54:28 -0400

    Update test_sound_clf.py

--- test_sound_clf.py
+++ test_sound_clf.py
@@ -1,17 +1,76 @@
 from gensim.models import KeyedVectors
 import numpy as np
 from sklearn import svm
-import sys, os
+import random, nltk, sys, os, re
 from gensim.scripts.glove2word2vec import glove2word2vec
 from sklearn.externals import joblib
-from sklearn.metrics import accuracy_score
 import pickle
 
 
 
+
+
+
+
+def process_doc(filename):
+    punctuation = '!"#$%&()*+,-./:;<=>?@[\\]^_{|}~'
+    temp_data = []
+    raw_file = open(filename, 'r')
+    data = ' '.join(raw_file.read().split())
+    findings = re.findall('sounds?\sof\s(\S+?\s\S+?)\s', data)
+
+    for phrase in findings:
+        tokens = nltk.word_tokenize(phrase)
+        tokens = nltk.pos_tag(tokens)
+
+        justtags = map(lambda x: x[1], tokens)
+        justtags = list(justtags)
+
+        if justtags[0] == 'DT':
+            justtags.pop(0)
+
+
+        phraselen = 0
+        if len(justtags) == 1:
+            if justtags[0] == 'VBG':
+                phraselen = 1
+            elif justtags[0] == 'NN' or justtags[0] == 'NNS':
+                phraselen = 1
+            else:
+                phraselen = 0
+        else:
+            if justtags[0] == 'VBG':
+                if justtags[1] == 'NN' or justtags[1] == 'NNS':
+                    phraselen = 2 #(DT) VBG NN(S)
+                else:
+                    phraselen = 1 # VBG
+
+            elif justtags[0] == 'NN' or justtags[0] == 'NNS':
+                if (justtags[1] =='NN' or justtags[1] == 'NNS') and justtags[0] == 'NN':
+                    phraselen = 2 # (DT) NN NN(S)
+                elif justtags[1] == 'VBG':
+                    phraselen = 2 # (DT) NN(S) VBG
+                else:
+                    phraselen = 1 # (DT) NN(S)
+
+            elif justtags[0] == 'JJ':
+                if justtags[1] =='NN' or justtags[1] == 'NNS':
+                    phraselen = 2 # (DT) JJ NN(S)
+            else:
+                phraselen = 0 # did not match
+
+        if phraselen != 0:
+            phrase = ' '.join(phrase.split()[:phraselen])
+            translator = str.maketrans('','',punctuation)
+            phrase = phrase.translate(translator)
+            temp_data.append(phrase)
+
+    return temp_data
+
+
+
 def vectorify(data, vector_model, glove):
-    temp_X = []
-    temp_y = []
+    temp_data = []
     vectors = []
 
     # goes through all training/testing examples
@@ -19,9 +78,8 @@ def vectorify(data, vector_model, glove):
 
         #makes sure dataline is not empty
         if example:
-            example = example.split(',')
-            sound = example[0].split()
-            correct = example[1]
+            sound = example.split()
+
 
             if len(sound) == 1:
                 sound += sound
@@ -54,12 +112,10 @@ def vectorify(data, vector_model, glove):
                 #concatenates vectors together
                 vector = list(one) + list(two)
                 sound = ' '.join(sound)
-                temp_y.append(int(correct))
-                temp_X.append(sound)
+                temp_data.append(sound)
                 vectors.append(vector)
 
-
-    return(temp_X, temp_y, vectors)
+    return(temp_data, vectors)
 
 
 
@@ -69,7 +125,7 @@ def vectorify(data, vector_model, glove):
 
 
 # Reads command line arguments
-if len(sys.argv) != 5:
+if len(sys.argv) != 6:
     sys.exit('Wrong number of arguments - ERROR')
 else:
     if sys.argv[1] == 'glove':
@@ -82,6 +138,15 @@ else:
 vectors_filename = sys.argv[2]
 clf_filename = sys.argv[3]
 test_filename = sys.argv[4]
+sentences = sys.argv[5].lower()
+
+if sentences == 'true':
+    sentences = True
+elif sentences == 'false':
+    sentences = False
+else:
+    sys.exit('ERROR - check data filetype (True/False)')
+
 
 
 
@@ -102,27 +167,34 @@ else:
 # Loads the SVM classifier from file
 print("Loading SVM sound classifier...")
 clf = pickle.load(open(clf_filename, 'rb'))
-#clf = joblib.load(clf_filename)
 
 
 
 # Processes test data
 print("Processing test data...")
-
-test = open(test_filename,'r')
-data = test.read().split('\n')
-
-
-
-
+if sentences:
+    data = process_doc(test_filename)
+else:
+    test = open(test_filename,'r')
+    data = test.read().split('\n')
 
 
-(X, y, vectors) = vectorify(data, vector_model, glove)
-assert(len(X) == len(y) and len(X) == len(vectors))
 
 
-num_sounds = len(X)
+(data,vectors) = vectorify(data, vector_model, glove)
+num_sounds = len(data)
 predictions = clf.predict(vectors)
-accuracy = accuracy_score(y,predictions)
+confidence = clf.decision_function(vectors)
+finalsounds = []
 
-print('Accuracy for the test set on the given model is: ' + str(accuracy))
+assert(len(confidence) == len(predictions))
+output_filename = 'results.txt'
+output = open(output_filename, 'w')
+for index in range(num_sounds):
+    if predictions[index]:
+        output.write(data[index] + ',' + str(confidence[index]) + '\n')
+
+output.close()
+
+
+print('List of sounds can be found in ' + output_filename)
